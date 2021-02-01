c7eaecadaff6198d5f2c1d2fb7070f7778143650
muhsin <manshadchangampalli010@gmail.com>
2021-02-01 11:30:14 +0530

    Addition sample created

---
+++ netBeansDay1/src/netbeansday1/NetBeansDay1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeansday1;
+
+/**
+ *
+ * @author ccf
+ */
+public class NetBeansDay1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
